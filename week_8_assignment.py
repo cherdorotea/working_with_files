75eda62d8d1d670f2df9ad9d66231cf8d11d4715
cherdorotea <71992076+cherdorotea@users.noreply.github.com>
2020-09-28 15:14:48 -0500

    Add files via upload
    Updated week 8 assignment.

---
+++ week_8_assignment.py
@@ -0,0 +1,21 @@
+import os
+
+def main():
+    directory = input("Enter the path to your file: ")
+    filename = input("Enter filename: ")
+    if os.path.isdir(directory): # this checks if the path is indeed in the directory
+        writeFile = open(os.path.join(directory,filename),'w') # creating and opening the file to write
+        name = input("What is your name? ")
+        address = input("What is your address? ")
+        phone_number = input("What is your phone number? ")
+        writeFile.write(name+", "+address+", "+phone_number+'\n') # this is the user's input, separated by a comma
+        writeFile.close() # the file will close after the writing is done
+        
+        print("File contents: ")
+        readFile = open(os.path.join(directory,filename),'r') # reading the file for proof of storing
+        for line in readFile:
+            print(line) # if storing is successful, this should display the user's input
+        readFile.close()
+    else:
+        print("Sorry. Path not located. Please rerun this module and try again.")
+main()
